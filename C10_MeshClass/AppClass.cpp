8b4f4f507e64a1c1e3a54b0bee2dc4974fd549d5
dab6084 <dab6084@rit.edu>
2020-02-08 11:53:45 -0500

    [E05] Completed the Space Invader, now just have to create movemetn across screen for it

--- C10_MeshClass/AppClass.cpp
+++ C10_MeshClass/AppClass.cpp
@@ -3,7 +3,7 @@ void Application::InitVariables(void)
 {
 	//Make MyMesh object
 	m_pMesh = new MyMesh();
-	m_pMesh->GenerateCube(1.0f, C_BROWN);
+	m_pMesh->GenerateCube(1.0f, C_BLACK);
 
 	//Make MyMesh object
 	//m_pMesh1 = new MyMesh();
@@ -29,12 +29,64 @@ void Application::Display(void)
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
 	matrix4 m4Model = ToMatrix4(m_qArcBall);
 
+#pragma region Space Invader Creation
 	m_pMesh->Render(m4Projection, m4View, m4Model);
-	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3 (1, 0, 0)));
-	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3 (2, 0, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3 (0, 1, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(0, 2, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(0, -1, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(1, -3, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(1, -1, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(1, 0, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(1, 1, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(1, 2, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-1, -3, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-1, -1, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-1, 0, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-1, 1, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-1, 2, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-2, -3, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-2, -1, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-2, 0, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-2, 2, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-2, 3, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(2, -3, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(2, -1, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(2, 0, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(2, 2, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(2, 3, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-3, -2, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-3, -1, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-3, 0, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-3, 1, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-3, 2, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-3, 4, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(3, -2, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(3, -1, 0)));
 	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(3, 0, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(3, 1, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(3, 2, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(3, 4, 0)));
 	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(4, 0, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(4, 1, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-4, 0, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-4, 1, 0)));
 	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(5, 0, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(5, -1, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(5, -2, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-5, 0, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-5, -1, 0)));
+	m_pMesh->Render(m4Projection, m4View, m4Model * glm::translate(vector3(-5, -2, 0)));
+#pragma endregion
+
+#pragma region Space Invader Movement
+#pragma endregion
+	
+
+	/*
+		- Create loop where Invader constantly moves to the right
+			- Make loop where it goes through fps to move?
+	*/
+	
 
 	// draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
